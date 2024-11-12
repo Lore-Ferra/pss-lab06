5ed06da23729b1b87a5aa6af5874adc6909ca5ed
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2024-11-12 10:47:16 +0100

    add new function @override chargeManagementFees and withdrawFromATM

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -8,9 +8,25 @@ public class ExtendedStrictBankAccount extends SimpleBankAccount {
     }
 
     @Override
-    public withdraw(final int id, final double amount){
+    public withdraw(final int id, final double amount) {
         if(isWithdrawAllowed(amount))
         super.withdraw(id, amount);
+    }
+
+    @Override
+    public void chargeManagementFees(final int id){
+        final double feeAmount = MANAGEMENT_FEE + getTransactionsCount() * TRANSACTION_FEE;
+        if (checkUser(id) && isWithdrawAllowed(feeAmount)) {
+            setBalance(getBalance() -= feeAmount);
+            resetTransactions(getTransactionsCount());
+        }
+    }
+
+    @Override
+    public void withdrawFromATM(final int id, final double amount){
+        if(isWithdrawAllowed(amount + ATM_TRANSACTION_FEE)){
+            super.withdrawFromATM(id, amount);
+        }
     }
 
     private boolean isWithdrawAllowed(final double amount) {
